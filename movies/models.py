d9adf07c2fe19924cd3179c36b9f68183ca2d573
KhalidNC <hussainkhalid70@yahoo.com>
2024-01-06 01:49:00 +0600

    Added model classes Genera and Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Genera(models.Model):
+  name = models.CharField(max_length=255)
+
+class Movie(models.Model):
+  title = models.CharField(max_length=255)
+  release_year = models.IntegerField()
+  number_in_stock = models.IntegerField()
+  daily_rate = models.FloatField()
+  genera = models.ForeignKey(Genera, on_delete=models.CASCADE)
