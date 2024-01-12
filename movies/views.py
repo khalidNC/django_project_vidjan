b41d2aa2c4d0260db9f79f86bc6dc88f992b0353
KhalidNC <hussainkhalid70@yahoo.com>
2024-01-13 02:00:13 +0600

    Modified with database abstraction api to display movie title on movies page

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,15 @@
+# Import Movie class from models module from current directory
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Define views usally the function called index that represents the main page of an app
 # When the request is sent to an address the the function is get called and returns response, this could be a text
 # So we import HttpResponse class from django http module
 def index(request):
-  return HttpResponse("Hello Word")
+  # Call all the movie object from database by database abstraction api and it returns movies object
+  movies = Movie.objects.all()
+  # Use list comprehention to retrive stirng out of the movie objects
+  # Use comma separator join method to display all the movie titles on the /movies page
+  output = ', '.join([m.title for m in movies])
+  return HttpResponse(output)
