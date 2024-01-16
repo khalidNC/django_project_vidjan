602e6356a5b0f7fe39974c6a6574c1f33461e660
KhalidNC <hussainkhalid70@yahoo.com>
2024-01-17 01:56:56 +0600

    Refactor index html and urls module using app_name varibale to set url name

--- movies/urls.py
+++ movies/urls.py
@@ -10,7 +10,13 @@ Also specify the name of module(index) with keyword:argument.
 # Add new path object to define new url parameter for movies/detail page
 # And name space set for the movie/id url as movies_detail and for index page movies_index
 # Use int: converter to convert movie_id to integer
+# Instead define url's name with prefix app's name, let's define a know variable app_name and assing app'e name in this case, movies.
+# and then delete 'movies_' from the url's name
+app_name = 'movies'
+
 urlpatterns = [
-  path("", views.index, name="movies_index"),
-  path("<int:movie_id>", views.detail, name="movies_detail")
+  path("", views.index, name="index"),
+  # path("", views.index, name="movies_index"),
+  path("<int:movie_id>", views.detail, name="detail")
+  # path("<int:movie_id>", views.detail, name="movies_detail")
 ]
