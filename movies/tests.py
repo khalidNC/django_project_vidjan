ba032da8948b391efe83fd0c6c0de4ce7dbd4d5f
KhalidNC <hussainkhalid70@yahoo.com>
2024-02-18 17:22:35 +0600

    Added a couple of unit tests for the model class

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . models import Genre, Movie
+
+class GenreTestCase(TestCase):
+    def test_model_field(self):
+        obj = Genre.objects.create(name="Test")
+        self.assertEqual(obj.name, "Test")
+
+class MovieTestCase(TestCase):
+    def test_movie_creation(self):
+        genre = Genre.objects.create(name="Action")
+        obj = Movie.objects.create(title="The Run", description="Indian action movie", release_year="1998", number_in_stock=1, daily_rate=1, genre=genre, date_created="2024-02-17", watch="www.youtube.com")
+        self.assertEqual(obj.title, "The Run")
+        self.assertEqual(obj.description, "Indian action movie")
+        self.assertEqual(obj.release_year, "1998")
+        self.assertEqual(obj.number_in_stock, 1)
+        self.assertEqual(obj.daily_rate, 1)
+        self.assertEqual(obj.genre, genre)
+        self.assertEqual(obj.date_created, "2024-02-17")
+        self.assertEqual(obj.watch, "www.youtube.com")
