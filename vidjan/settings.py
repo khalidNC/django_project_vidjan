09c4ac355482a4538f251ac1855d6decfe078d05
KhalidNC <hussainkhalid70@yahoo.com>
2024-02-25 17:26:21 +0600

    Deleted staging database and updated settings

--- vidjan/settings.py
+++ vidjan/settings.py
@@ -97,12 +97,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get("STAGING"):
-   DATABASES['default'] = {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'testing.db'),  # Separate SQLite database for staging
-   }
-
 # Created a class instance for dj-database-url and call parse method that takes external url of postgresql as string
 # And the the instance rerunrs default database and do migrate
 # DATABASES["default"] = dj_database_url.parse("external_connection_url")
