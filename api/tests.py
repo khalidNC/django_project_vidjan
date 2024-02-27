6de61b295338986202c831dca0a113fff5bdd1b7
KhalidNC <hussainkhalid70@yahoo.com>
2024-02-27 23:20:48 +0600

    Added api tests and updated yml file to run the test on github actions

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,12 @@
 from django.test import TestCase
-
 from rest_framework.test import APIClient
+from rest_framework import status
+from movies.models import Movie, Genre
 
-# Create your tests here.
+class MovieAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_movies_list(self):
+        response = self.client.get('/api/movies/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
